d9edbbd25fd69cb0ff7ea0bd78b8ff34a0b0730f
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-09 02:22:48 +0530

    shadcn ui theme frustration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,37 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { CgMenuRight } from "react-icons/cg";
+import { TbMenu } from "react-icons/tb";
 function Header() {
   return (
     <div className='flex justify-between w-full h-full items-center overflow-hidden'>
-        <div className='rounded-md font-bold text-left text-white text-2xl '>
+        <div className='rounded-md font-bold text-left  text-lg lg:text-2xl '>
             Webmastery.pro
         </div>
-        <div className='flex gap-0'>
-            <div className='text-white bg-transparent hover:bg-white duration-500 rounded-full cursor-pointer hover:text-black font-medium px-10 py-2 hover:shadow-lg hover:shadow-black'>
+        <div className='hidden lg:flex gap-0'>
+            <div className=' bg-transparent hover:bg-white duration-500 rounded-full cursor-pointer hover:text-black font-medium px-10 py-2 hover:shadow-lg hover:shadow-black'>
                 <h1>Components</h1>
             </div>
-            <div className='text-white bg-transparent hover:bg-white duration-500 rounded-full cursor-pointer hover:text-black font-medium px-10 py-2 hover:shadow-lg hover:shadow-black'>
+            <div className=' bg-transparent hover:bg-white duration-500 rounded-full cursor-pointer hover:text-black font-medium px-10 py-2 hover:shadow-lg hover:shadow-black'>
                 <h1>Documentation</h1>
             </div>
-            <div className='text-white bg-transparent hover:bg-white duration-500 rounded-full cursor-pointer hover:text-black font-medium px-10 py-2 hover:shadow-lg hover:shadow-black'>
+            <div className=' bg-transparent hover:bg-white duration-500 rounded-full cursor-pointer hover:text-black font-medium px-10 py-2 hover:shadow-lg hover:shadow-black'>
                 <h1>Projects</h1>
             </div>
-            <div className='text-white bg-transparent hover:bg-white duration-500 rounded-full cursor-pointer hover:text-black font-medium px-10 py-2 hover:shadow-lg hover:shadow-black'>
+            <div className=' bg-transparent hover:bg-white duration-500 rounded-full cursor-pointer hover:text-black font-medium px-10 py-2 hover:shadow-lg hover:shadow-black'>
                 <h1>Landing Pages</h1>
             </div>
         </div>
 
-        <div>
-            <Button className='bg-white text-black px-8'>SignUp</Button>
+        <div className='hidden lg:flex'>
+            <Button className='bg-white text-black lg:px-8  text-sm rounded-full'>SignUp</Button>
+        </div>
+        {/* mobile enu */}
+        <div className='lg:hidden  text-3xl'>
+            {/* <HiOutlineMenuAlt3 /> */}
+            {/* <CgMenuRight /> */}
+            <TbMenu />
         </div>
     </div>
   )
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,13 +6,13 @@ function Hero() {
     return (
         <div className="flex lg:flex-nowrap flex-wrap">
             <div className="w-full lg:w-[60%] px-5 lg:px-10 lg:py-10  ">
-                <h1 className="text-4xl lg:text-7xl font-extrabold text-white text-center">Introduction to Shad-cn</h1>
-                <p className="py-8 pb-6 px-2 lg:px-10 text-[16px] text-white">shadcn/ui is a set of beautifully-designed, accessible components and a code distribution platform. Works with your favorite frameworks and AI models. Open Source. Open Code. Every component in shadcn/ui shares a common, composable interface. If a component does not exist, we bring it in, make it composable, and adjust its style to match and work with the rest of the design system.
+                <h1 className="text-5xl lg:text-7xl font-extrabold text-center">Introduction to Shad-cn</h1>
+                <p className="py-8 pb-6 px-2 lg:px-10 text-[16px]">shadcn/ui is a set of beautifully-designed, accessible components and a code distribution platform. Works with your favorite frameworks and AI models. Open Source. Open Code. Every component in shadcn/ui shares a common, composable interface. If a component does not exist, we bring it in, make it composable, and adjust its style to match and work with the rest of the design system.
                 </p>
-                <p className="py-6 pb-6 px-2 lg:px-10 text-[16px] text-white">The design of shadcn/ui makes it easy for AI tools to work with your code. Its open code and consistent API allow AI models to read, understand, and even generate new components.An AI model can learn how your components work and suggest improvements or even create new components that integrate with your existing design.</p>
+                <p className="py-6 pb-6 px-2 lg:px-10 text-[16px]">The design of shadcn/ui makes it easy for AI tools to work with your code. Its open code and consistent API allow AI models to read, understand, and even generate new components.An AI model can learn how your components work and suggest improvements or even create new components that integrate with your existing design.</p>
                 <div className="flex flex-wrap lg:flex-nowrap gap-6 py-6 lg:py-10 justify-evenly lg:gap-20">
-                    <Button className='w-full lg:w-auto bg-white text-black font-black px-14 py-7'>Documentation</Button>
-                    <Button className='w-full lg:w-auto bg-white text-black font-black px-14 py-7'>Projects</Button>
+                    <Button className='w-full lg:w-auto cursor-pointer hover:bg-gray-200 duration-500 bg-white text-black font-black px-14 py-7'>Documentation</Button>
+                    <Button className='w-full lg:w-auto cursor-pointer hover:bg-gray-200 duration-500 bg-white text-black font-black px-14 py-7'>Live Projects</Button>
                 </div>
             </div> <br />
             <div className="w-[100%] lg:w-[40%] px-4 py-4">
